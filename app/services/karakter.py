73e48ac90761d390a4b0d559f9a7426d73185ba6
ThomasB332 <153505047+ThomasJB332@users.noreply.github.com>
2023-12-22 10:19:18 +0100

    finished query

--- app/services/karakter.py
+++ app/services/karakter.py
@@ -7,7 +7,13 @@ cursor = connectDBAndGiveCursor()
 def vindVerhaalLijnKarakter(karakter_naam):
     try:
         #do query on DB and use psycopg2.sql to mitigate the risk of sql injection
-        cursor.execute(sql.SQL(""))
+        cursor.execute(sql.SQL("SELECT * FROM karakter k WHERE k.naam = %s \
+                               INNER JOIN stripboek_karakter SK ON K.naam = SK.naam \
+                               INNER JOIN stripboek S ON SK.naam = S.naam AND SK.issueNummer = S.Issuenummer \
+                               ORDER BY S.uitgavedatum ASC"), (karakter_naam))
+        output = cursor.fetchall()
+        return(output)
     except psycopg2.Error as e:
         print(f"Error handeling sql query: {e}")
         return False
+    
