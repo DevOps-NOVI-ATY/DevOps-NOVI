88d22df2b630f399fc2479dad84be09511d009c7
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-09 15:32:09 +0100

    meer testdata 2 karakters 3 series

--- app/models/populateDatabase.py
+++ app/models/populateDatabase.py
@@ -6,47 +6,72 @@ def populateDatabase(engine):
     Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     session = Session()
     # 1. Add records to the Uitgever table
-    uitgever1 = Uitgever(naam='Uitgever1')
-    session.add(uitgever1)
+    marvel = Uitgever(naam='Marvel Comics')
+    session.add(marvel)
 
     # 2. Add records to the Serie table
-    serie1 = Serie(naam='Serie1', serieGrootte=10, uitgever=uitgever1)
-    session.add(serie1)
+    spiderman5min = Serie(naam='5 Minute Spider-Man Stories HC', serieGrootte=1, uitgever=marvel)
+    session.add(spiderman5min)
+
+    spidermanADV = Serie(naam='The Adventures of Spider-Man', serieGrootte=14, uitgever=marvel)
+    session.add(spidermanADV)
+
+    spidermanHulk = Serie(naam='Amazing Spider-Man Dallas Times Herald Giveaway', serieGrootte=5, uitgever=marvel)
+    session.add(spidermanHulk)
 
     # 3. Add records to the Stripboek table
-    stripboek1 = Stripboek(naam='Stripboek1', issueNummer=1, Uitgavedatum='2022-01-01', paginas=100, prijs=19.99)
-    stripboek1.series.append(serie1)  # Link the stripboek to the serie
-    session.add(stripboek1)
+    spiderman5MinStrip = Stripboek(naam='5 Minute Spider-Man Stories HC', issueNummer=1, Uitgavedatum='2017-06-21', paginas=192, prijs=12.99)
+    spiderman5MinStrip.series.append(spiderman5min)  # Link the stripboek to the serie
+    spiderman5MinStrip.karakters.append(spiderman)
+    spiderman5MinStrip.covers.append(hardCover)
+    session.add(spiderman5MinStrip)
+
+    spidermanStrip1 = Stripboek(naam='The Adventures of Spider-Man #1', issueNummer=1, Uitgavedatum='1996-04-03', paginas=36, prijs=.99)
+    spidermanStrip1.series.append(spidermanADV)  # Link the stripboek to the serie
+    spidermanStrip1.karakters.append(spiderman)
+    spidermanStrip1.covers.append(softCover)
+    session.add(spidermanStrip1)
+
+    spidermanStrip2 = Stripboek(naam='The Adventures of Spider-Man #2', issueNummer=1, Uitgavedatum='1996-04-03', paginas=36, prijs=.99)
+    spidermanStrip2.series.append(spidermanADV)  # Link the stripboek to the serie]
+    spidermanStrip2.karakters.append(spiderman)
+    spidermanStrip2.covers.append(softCover)
+    session.add(spidermanStrip2)
+
+    spidermanStrip3 = Stripboek(naam='The Adventures of Spider-Man #3', issueNummer=1, Uitgavedatum='1996-04-03', paginas=36, prijs=.99)
+    spidermanStrip3.series.append(spidermanADV)  # Link the stripboek to the serie
+    spidermanStrip3.karakters.append(spiderman)
+    spidermanStrip3.covers.append(softCover)
+    session.add(spidermanStrip3)
+
+    spidermanHulkStrip1 = Stripboek(naam='Amazing Spider-Man Dallas Times Herald Giveaway #1', issueNummer=1, Uitgavedatum='1981-08-05', paginas=32, prijs=0)
+    spidermanHulkStrip1.series.append(spidermanHulk)  # Link the stripboek to the serie
+    spidermanHulkStrip1.karakters.append(spiderman)
+    spidermanHulkStrip1.karakters.append(hulk)
+    spidermanHulkStrip1.covers.append(softCover)
+    session.add(spidermanHulkStrip1)
+
+    spidermanHulkStrip2 = Stripboek(naam='Amazing Spider-Man Dallas Times Herald Giveaway #2', issueNummer=1, Uitgavedatum='1982-06-02', paginas=32, prijs=0)
+    spidermanHulkStrip2.series.append(spidermanHulk)  # Link the stripboek to the serie
+    spidermanHulkStrip2.karakters.append(spiderman)
+    spidermanHulkStrip2.karakters.append(hulk)
+    spidermanHulkStrip2.covers.append(softCover)
+    session.add(spidermanHulkStrip2)
 
     # 4. Add records to the Karakter table
-    karakter1 = Karakter(naam='Karakter1')
-    session.add(karakter1)
-    # 4. Add records to the Karakter table
-    karakter2 = Karakter(naam='Karakter12')
-    session.add(karakter2)
-    # 4. Add records to the Karakter table
-    karakter3 = Karakter(naam='Karakter123')
-    session.add(karakter3)
+    spiderman = Karakter(naam='spiderman')
+    session.add(spiderman)
+    
+    hulk = Karakter(naam='hulk')
+    session.add(hulk)
 
     # 5. Add records to the Cover_soort table
-    cover_soort1 = Cover_soort(naam='CoverSoort1')
-    session.add(cover_soort1)
+    hardCover = Cover_soort(naam='hardCover')
+    session.add(hardCover)
+
+    softCover = Cover_soort(naam='softCover')
+    session.add(softCover)
+
 
     # Commit the changes to the database
     session.commit()
-
-    # Query the database to verify the records have been added
-    uitgever_query = session.query(Uitgever).first()
-    print("Uitgever:", uitgever_query.naam)
-
-    serie_query = session.query(Serie).first()
-    print("Serie:", serie_query.naam)
-
-    stripboek_query = session.query(Stripboek).first()
-    print("Stripboek:", stripboek_query.naam)
-
-    karakter_query = session.query(Karakter).first()
-    print("Karakter:", karakter_query.naam)
-
-    cover_soort_query = session.query(Cover_soort).first()
-    print("Cover Soort:", cover_soort_query.naam)
