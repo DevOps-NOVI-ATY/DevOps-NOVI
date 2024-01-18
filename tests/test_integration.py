8a43e61f5a615b7740fa73c6ed1ed8d4e0c636e9
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-18 16:05:07 +0100

    fixed integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,11 +5,11 @@ from fastapi.testclient import TestClient
 from .test_main import client
 
 def test_karakter_status_code():
-    response = client.get("/karakter/")
+    response = client.get("/karakters/")
     assert response.status_code == 200
 
 def test_karakter_response():
-    response = client.get("/karakter/")
+    response = client.get("/karakters/")
     assert response.json() == [{"Karakter":{"naam":"spider-man"}},{"Karakter":{"naam":"x-men"}},
                                {"Karakter":{"naam":"avengers"}},{"Karakter":{"naam":"iron man"}},
                                {"Karakter":{"naam":"thor"}},{"Karakter":{"naam":"black widow"}},
