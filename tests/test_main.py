607574e87a647a13ceef253402e6e0ab7fe1327b
yb-novi <153506031+yb-novi@users.noreply.github.com>
2024-01-18 14:18:22 +0100

    jklsdf
    dfasdf

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,6 +10,8 @@ def mock_database_url(monkeypatch):
     monkeypatch.setenv('DATABASE_URL', 'test_database_url')
     assert os.environ.get('DATABASE_URL') == 'test_database_url'
 
+
+
 # Use the TestClient with the fixture
 def test_status_code(mock_database_url):
     
@@ -21,4 +23,3 @@ def test_response(mock_database_url):
     response = client.get("/")
     assert response.json() == {"greeting": "Hello world this is the new file"}
 
-    
