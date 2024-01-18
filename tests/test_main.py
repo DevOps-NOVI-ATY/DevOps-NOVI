401baf994b9680380818601c43fc1877905d641f
yb-novi <153506031+yb-novi@users.noreply.github.com>
2024-01-18 14:10:04 +0100

    niet mergen
    daf

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,3 +20,5 @@ def test_status_code(mock_database_url):
 def test_response(mock_database_url):
     response = client.get("/")
     assert response.json() == {"greeting": "Hello world this is the new file"}
+
+    
