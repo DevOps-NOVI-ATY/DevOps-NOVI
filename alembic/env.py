1beed179f5f359b261ff34a2ef74d62f6fee5dee
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-25 11:52:09 +0100

    Alembic working + first revision

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,12 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 
 from alembic import context
+import os
+
+db_url = os.environ.get('DATABASE_URL', 'postgresql://user-name:strong-password@localhost:5433/api')
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models.models import Base, Uitgever, Serie, Stripboek, Karakter, Cover_soort, Serie_strip, Strip_kar, Strip_cover
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +42,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    #url = config.get_main_option("sqlalchemy.url")
+    url = db_url
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,11 +62,13 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section, {}),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
+
+    connectable = create_engine(db_url)
 
     with connectable.connect() as connection:
         context.configure(
--- dockerfile
+++ dockerfile
@@ -16,4 +16,4 @@ EXPOSE 8000
 EXPOSE 5432
 
 # FastAPI runnen op een uvicorn server
-CMD ["uvicorn", "app.main:app", "--host=0.0.0.0", "--port=8000"]
+ENTRYPOINT [ "entrypoint.sh" ]
