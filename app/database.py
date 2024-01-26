3ab4b299bd8b0e762ad8e49ae918382048503274
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-26 12:55:33 +0100

    upload without populating DB

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ from sqlalchemy import create_engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #Controle op db. als het niet bestaat, maakt het de db aan.
-from app.models.populateDatabase import populateDatabase, csv_to_db
+# from app.models.populateDatabase import populateDatabase, csv_to_db
 
 if not database_exists(engine.url):
     create_database(engine.url)
@@ -23,5 +23,5 @@ Base.metadata.create_all(engine)
 #populateDatabase(engine) 
 
 #database aanvullen met csv file
-csv_to_db("app/models/dataset.csv")
+# csv_to_db("app/models/dataset.csv")
 
