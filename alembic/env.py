67cab03869be8198d2daf4f7c70aac7abc79ba34
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-29 15:16:25 +0100

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -21,8 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.models.models import Base, Uitgever, Serie, Stripboek, Karakter, Cover_soort, Serie_strip, Strip_kar, Strip_cover
-target_metadata = Base.metadata
+from app.models.models import base, publishers, series, comics, characters, covers, series_comics, comics_char, comics_covers
+target_metadata = base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
