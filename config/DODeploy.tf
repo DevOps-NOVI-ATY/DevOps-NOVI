45cbac41a296d1613c38bd4cb10f4cf728937fd6
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-30 14:06:31 +0100

    add DO pods and loadbalancer

--- config/DODeploy.tf
+++ config/DODeploy.tf
@@ -30,72 +30,64 @@ resource "digitalocean_kubernetes_cluster" "kubernetes-api-cluster" {
     size       = "s-2vcpu-2gb"
     auto_scale = true
     min_nodes  = 1
-    max_nodes  = 4
+    max_nodes  = 
   }
 }
 
-resource "kubernetes_namespace" "toepassing" {
+resource "kubernetes_namespace" "api" {
     metadata {
         name = "novi-api"
     }
 }
 
-resource "kubernetes_deployment" "noviAPI"{
-        metadata{
-            name = "novi-api"
-            labels = {
-                test = "novi-api-tag"
-            }
-            namespace = "novi-api"
-        }
-        spec {
-            replicas = 5
+resource "kubernetes_deployment" "noviAPI" {
+	metadata {
+		name      = "novi-api-deployment"
+		namespace = kubernetes_namespace.api.metadata[0].name
+	}
 
-            selector {
-                match_labels = {
-                    test = "novi-api-tag"
-                }
-            }
-        template {
-            metadata {
-                labels = {
-                    test = "novi-api-tag"
+	spec {
+		replicas = 5
 
-                }
-            }
+		selector {
+		match_labels = {
+			app = "novi-api"
+		}
+		}
 
-            spec {
-                container{
-                    image = "registry.digitalocean.com/dev-ops-novi-api/api"
-                    image_pull_policy = "IfNotPresent"
-                    name = "devops-novi-api"
-                    env {
+		template {
+			metadata {
+				labels = {
+				app = "novi-api"
+				}
+			}
+
+			spec {
+				container{
+					image = "registry.digitalocean.com/dev-ops-novi-api/api"
+					image_pull_policy = "IfNotPresent"
+					name = "devops-novi-api"
+					env {
 						name  = "DATABASE_URL"
 						value = var.DATABASE_URL
-                    }
-                }
-            }
-        }
-    }
+					}
+				}
+			}
+		}
+	}
 }
 
-resource "kubernetes_service" "LoadBalancer" {
-  metadata {
-      name = "fastapi"
-      namespace = "novi-api"
-    }
+resource "digitalocean_loadbalancer" "lb" {
+  name   = "novi-api-lb"
+  region = "ams3"
 
-spec {
-    selector = {
-          test = "novi-api-tag"
-    }
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
 
-    port {
-      port = 80
-      target_port = 8000
-
-      }
-    type = "LoadBalancer"
+    target_port     = 8000
+    target_protocol = "http"
   }
 
+  droplet_ids = digitalocean_kubernetes_cluster.kubernetes-api-cluster.node_pool.*.id
 }
