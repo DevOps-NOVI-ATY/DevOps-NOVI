c6b18c609e1b719e071392738b31c547c12a447a
ThomasJB332 <153505047+ThomasJB332@users.noreply.github.com>
2024-01-31 13:03:25 +0100

    check and make container registery

--- config/DODeploy.tf
+++ config/DODeploy.tf
@@ -20,6 +20,17 @@ variable "DATABASE_CLUSTER_NAME" {
   type        = string
 }
 
+variable "CREATE_NEW_CONTAINER_REGISTRY" {
+  description = "Set to true if you want to create a new Container Registry"
+  type        = bool
+  default     = true
+}
+
+variable "CONTAINER_REGISTRY_NAME" {
+  description = "Container Registry name"
+  type        = string
+}
+
 variable "DIGITALOCEAN_ACCESS_TOKEN" {}
 
 terraform {
@@ -70,4 +81,17 @@ resource "digitalocean_database_cluster" "database-cluster" {
   size           = "db-s-1vcpu-1gb"
   region         = "ams3"
   node_count     = var.CREATE_NEW_DATABASE_CLUSTER ? 1 : 0
+  tags       	 = ["api"]
+}
+
+data "digitalocean_container_registry" "existing_registry" {
+  count = var.CREATE_NEW_CONTAINER_REGISTRY ? 0 : 1
+  name  = var.CONTAINER_REGISTRY_NAME
+}
+
+resource "digitalocean_container_registry" "container-registry" {
+  count = var.CREATE_NEW_CONTAINER_REGISTRY ? 1 : 0
+  name  = var.CONTAINER_REGISTRY_NAME
+  region = "ams3"
+  subscription_tier = "basic"
 }
