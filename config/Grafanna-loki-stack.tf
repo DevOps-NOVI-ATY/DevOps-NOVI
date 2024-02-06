243811ba71101d2a6d117e740019d8eef3d56962
AdriaandeWit <119049095+AdriaandeWit@users.noreply.github.com>
2024-02-06 15:11:54 +0100

    provider fix

--- config/Grafanna-loki-stack.tf
+++ config/Grafanna-loki-stack.tf
@@ -7,13 +7,13 @@ data "digitalocean_kubernetes_cluster" "kubernetes-api-cluster"{
 
 provider "kubernetes" {
 
-  host  = data.digitalocean_kubernetes_cluster.kubernetes-api-cluster.endpoint
+  host  = data.digitalocean_kubernetes_cluster.kubernetes-api-cluster[0].endpoint
 
-  token = data.digitalocean_kubernetes_cluster.kubernetes-api-cluster.kube_config[0].token
+  token = data.digitalocean_kubernetes_cluster.kubernetes-api-cluster[0].kube_config[0].token
 
   cluster_ca_certificate = base64decode(
 
-    data.digitalocean_kubernetes_cluster.kubernetes-api-cluster.kube_config[0].cluster_ca_certificate
+    data.digitalocean_kubernetes_cluster.kubernetes-api-cluster[0].kube_config[0].cluster_ca_certificate
 
   )
 
