f92fe9f7d769f768eeb1b74414b55d566a3dbac1
AdriaandeWit <119049095+AdriaandeWit@users.noreply.github.com>
2024-02-07 14:19:39 +0100

    Add: all the grafana-loki to DoDeploy file DELETE: The grafana-loki-stack

--- config/DODeploy.tf
+++ config/DODeploy.tf
@@ -49,6 +49,28 @@ terraform {
 provider "digitalocean" {
   token = var.DIGITALOCEAN_ACCESS_TOKEN
 }
+provider "kubernetes" {
+  host  = data.digitalocean_kubernetes_cluster.kubernetes-api-cluster.endpoint
+  token = data.digitalocean_kubernetes_cluster.kubernetes-api-cluster.kube_config[0].token
+
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.kubernetes-api-cluster.kube_config[0].cluster_ca_certificate
+  )
+
+}
+ 
+provider "helm" {
+
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.kubernetes-api-cluster[0].endpoint
+    token = digitalocean_kubernetes_cluster.kubernetes-api-cluster[0].kube_config[0].token
+    
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.kubernetes-api-cluster[0].kube_config[0].cluster_ca_certificate
+    )
+  }
+
+}
 
 data "digitalocean_kubernetes_cluster" "existing_kubernetes_cluster" {
   count = var.CREATE_NEW_KUBERNETES_CLUSTER ? 0 : 1
@@ -102,4 +124,33 @@ resource "digitalocean_container_registry" "container-registry" {
   subscription_tier_slug = "basic"
 }
 
+resource "kubernetes_namespace" "loki-stack" {
+  metadata {
+    annotations = {
+      name = "loki-stack"
+    }
+  
+    name = "loki-stack"
+  }
+    depends_on = [digitalocean_kubernetes_cluster.kubernetes-api-cluster]
+}
 
+
+  resource "helm_release" "loki" {
+    name       = "loki"
+    repository = "https://grafana.github.io/helm-charts"
+    chart      = "loki-stack"
+    version    = "2.10.1"
+    namespace = "loki-stack"
+  
+    set {
+      name  = "grafana.enabled"
+      value = "true"
+    }
+  
+    set {
+      name  = "promtail.enabled"
+      value = "true"
+    }
+    depends_on = [kubernetes_namespace.loki-stack]
+  }
